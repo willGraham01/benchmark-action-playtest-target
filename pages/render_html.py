e6656cdbe34b672a23d48ed44cbacfd9331590a3
willGraham01 <1willgraham@gmail.com>
2023-07-12 11:40:36 +0100

    Write pyis to html converter

---
+++ pages/render_html.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from pyinstrument.session import Session
+from pyinstrument.renderers import HTMLRenderer
+
+
+def render_html(pyis_in: str | Path, html_out: str | Path):
+    """
+    Renders a pyis session file as HTML for visual output.
+    """
+    pyi_session = Session.load(pyis_in)
+
+    renderer = HTMLRenderer(show_all=False, timeline=False)
+    print(f"Writing {html_out}", end="...", flush=True)
+    with open(html_out, "w") as f:
+        f.write(renderer.render(pyi_session))
+    print("done")
+    return
